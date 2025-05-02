bd939e29dea7935bcc69bfcae26b01bb4dab37e6
jaewan shin <wodhks6307@naver.com>
2025-05-02 16:01:30 +0900

    자바 코딩테스트

---
+++ java-basic/src/CodingTest/Class03/P10986_005.java
@@ -0,0 +1,7 @@
+package CodingTest.Class03;
+
+public class P10986_005 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ java-basic/src/CodingTest/Class03/P1253_008.java
@@ -0,0 +1,7 @@
+package CodingTest.Class03;
+
+public class P1253_008 {
+    public static void main(String[] args) {
+
+    }
+}
